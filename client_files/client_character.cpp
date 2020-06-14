7bdbc498d6910be9096933ae71a57467efe8240b
Hernan Tain <hernantain@gmail.com>
2020-06-14 15:38:11 -0300

    Agrego fotos

--- client_files/client_character.cpp
+++ client_files/client_character.cpp
@@ -19,8 +19,8 @@
 Character::Character() : 
 	bodyPosX(0), 
 	bodyPosY(25), 
-	headPosX(3),
-	headPosY(13),
+	headPosX(4),
+	headPosY(15),
 	velX(0), 
 	velY(0), 
 	frame(0), 
@@ -32,7 +32,7 @@ Character::Character() :
 bool Character::load_images(SDL_Renderer* gRenderer) {
 
 	//Load sprite sheet texture
-	if( !this->bodyTexture.loadFromFile( "images/character.png", gRenderer ) ) {
+	if( !this->bodyTexture.loadFromFile( "images/armadura_placas_eg.png", gRenderer ) ) {
 		printf( "Failed to load walking animation texture!\n" );
 		return false;
 	}
@@ -41,7 +41,22 @@ bool Character::load_images(SDL_Renderer* gRenderer) {
 		printf( "Failed to load head texture!\n" );
 		return false;
 	}
+
+	if (!this->helmetTexture.loadFromFile("images/casco_hierro.png", gRenderer)) {
+		printf( "Failed to load head texture!\n" );
+		return false;
+	}
+
+	if (!this->tallTexture.loadFromFile("images/armadura_placas_he.png", gRenderer)) {
+		printf( "Failed to load head texture!\n" );
+		return false;
+	}
 	
+	this->loadTallWalkingFrontSprite();
+	this->loadTallWalkingBackSprite();
+	this->loadTallWalkingLeftSprite();
+	this->loadTallWalkingRightSprite();
+	this->loadHelmetSprite();
 	this->loadHeadSprite();
 	this->load_front_walking_sprite();
 	this->load_back_walking_sprite();
@@ -105,8 +120,7 @@ ProtocolMessage Character::handleEvent( SDL_Event& e ) {
 		// CODIGO PARA CUANDO QUERRAMOS MANEJAR EL CLICK DEL PERSONAJE
 		// int x, y; 
 		// SDL_GetMouseState( &x, &y ); 
-		// Event * ev = new MouseClickEvent(x,y);
-		// this->pq.push(ev);
+		// std::cout << "CLICK EN: " << x << " Y EN: " << y << std::endl; 
 	}
 
 	ProtocolMessage msg(
@@ -137,34 +151,51 @@ void Character::render(SDL_Renderer* gRenderer) {
 	std::unique_lock<std::mutex> lock(this->m);
 	//Show Character
 	if (orientation == RIGHT) {
-		SDL_Rect* currentClip = &this->walkingRightCharacter[ this->frame / 5 ];
+		// SDL_Rect* currentClip = &this->walkingRightCharacter[ this->frame / 5 ];
+		SDL_Rect* tallClip = &this->tallWalkingRightCharacter[ this->frame / 5 ];
 		SDL_Rect* headClip = &this->headOrientations[1];
+		SDL_Rect* helmetClip = &this->helmetOrientations[1];
 		this->headTexture.render(headPosX, headPosY, gRenderer, headClip);
-		this->bodyTexture.render(bodyPosX, bodyPosY, gRenderer, currentClip);
-
+		this->helmetTexture.render(headPosX, headPosY, gRenderer, helmetClip);
+		//this->bodyTexture.render(bodyPosX, bodyPosY, gRenderer, currentClip);
+		this->tallTexture.render(bodyPosX, bodyPosY, gRenderer, tallClip);
 	} else if(orientation == LEFT)  {
-		SDL_Rect *currentClip = &this->walkingLeftCharacter[ this->frame / 5 ];
+		// SDL_Rect *currentClip = &this->walkingLeftCharacter[ this->frame / 5 ];
+		SDL_Rect* tallClip = &this->tallWalkingLeftCharacter[ this->frame / 5 ];
 		SDL_Rect* headClip = &this->headOrientations[2];
+		SDL_Rect* helmetClip = &this->helmetOrientations[2];
 		this->headTexture.render(headPosX, headPosY, gRenderer, headClip);
-		this->bodyTexture.render(bodyPosX, bodyPosY, gRenderer, currentClip);
-
+		this->helmetTexture.render(headPosX, headPosY, gRenderer, helmetClip);
+		// this->bodyTexture.render(bodyPosX, bodyPosY, gRenderer, currentClip);
+		this->tallTexture.render(bodyPosX, bodyPosY, gRenderer, tallClip);
 	} else if(orientation == UP)  {
-		SDL_Rect *currentClip = &this->walkingBackCharacter[ this->frame / 5 ];
+		// SDL_Rect *currentClip = &this->walkingBackCharacter[ this->frame / 5 ];
+		SDL_Rect* tallClip = &this->tallWalkingBackCharacter[ this->frame / 5 ];
 		SDL_Rect* headClip = &this->headOrientations[3];
+		SDL_Rect* helmetClip = &this->helmetOrientations[3];
 		this->headTexture.render(headPosX, headPosY, gRenderer, headClip);
-		this->bodyTexture.render(bodyPosX, bodyPosY, gRenderer, currentClip);
-
+		this->helmetTexture.render(headPosX-1, headPosY-1, gRenderer, helmetClip);
+		// this->bodyTexture.render(bodyPosX, bodyPosY, gRenderer, currentClip);
+		this->tallTexture.render(bodyPosX, bodyPosY, gRenderer, tallClip);
 	} else if(orientation == DOWN)  {
-		SDL_Rect *currentClip = &this->walkingFrontCharacter[ this->frame / 5 ];
+		// SDL_Rect *currentClip = &this->walkingFrontCharacter[ this->frame / 5 ];
+		SDL_Rect* tallClip = &this->tallWalkingFrontCharacter[ this->frame / 5 ];
 		SDL_Rect* headClip = &this->headOrientations[0];
+		SDL_Rect* helmetClip = &this->helmetOrientations[0];
+		this->tallTexture.render(bodyPosX, bodyPosY, gRenderer, tallClip);
 		this->headTexture.render(headPosX, headPosY, gRenderer, headClip);
-		this->bodyTexture.render(bodyPosX, bodyPosY, gRenderer, currentClip);
+		this->helmetTexture.render(headPosX-1, headPosY-1, gRenderer, helmetClip);
+		// this->bodyTexture.render(bodyPosX, bodyPosY, gRenderer, currentClip);
 
 	} else {
-		SDL_Rect* currentClip = &this->walkingFrontCharacter[0];
+		// SDL_Rect* currentClip = &this->walkingFrontCharacter[0];
+		SDL_Rect* tallClip = &this->tallWalkingFrontCharacter[0];
 		SDL_Rect* headClip = &this->headOrientations[0];
+		SDL_Rect* helmetClip = &this->helmetOrientations[0];
+		this->tallTexture.render(bodyPosX, bodyPosY, gRenderer, tallClip);
 		this->headTexture.render(headPosX, headPosY, gRenderer, headClip);
-		this->bodyTexture.render( bodyPosX, bodyPosY, gRenderer, currentClip);
+		this->helmetTexture.render(headPosX-1, headPosY-1, gRenderer, helmetClip);
+		// this->bodyTexture.render( bodyPosX, bodyPosY, gRenderer, currentClip);
 	}
 	this->frame++;
 }
@@ -179,14 +210,6 @@ void Character::update_frames() {
 	if (this->frame / 5 >= 5)
 		this->frame = 0;
 }
-
-
-void Character::get_position() {
-	std::cout << "X: " << this->bodyPosX << " // Y: " << this->bodyPosY << std::endl;
-}
-
-
-
 
 
 /**
@@ -343,4 +366,149 @@ void Character::loadHeadSprite() {
 	this->headOrientations[3].y = 0;
 	this->headOrientations[3].w = 15;
 	this->headOrientations[3].h = 17;
+}
+
+void Character::loadHelmetSprite() {
+	this->helmetOrientations[0].x = 0;
+	this->helmetOrientations[0].y = 0;
+	this->helmetOrientations[0].w = 17;
+	this->helmetOrientations[0].h = 17;
+
+	this->helmetOrientations[1].x = 17;
+	this->helmetOrientations[1].y = 0;
+	this->helmetOrientations[1].w = 17;
+	this->helmetOrientations[1].h = 17;
+
+	this->helmetOrientations[2].x = 34;
+	this->helmetOrientations[2].y = 0;
+	this->helmetOrientations[2].w = 17;
+	this->helmetOrientations[2].h = 17;
+
+	this->helmetOrientations[3].x = 51;
+	this->helmetOrientations[3].y = 0;
+	this->helmetOrientations[3].w = 17;
+	this->helmetOrientations[3].h = 17;
+}
+
+
+void Character::loadTallWalkingFrontSprite() {
+	this->tallWalkingFrontCharacter[0].x = 1;
+	this->tallWalkingFrontCharacter[0].y = 2;
+	this->tallWalkingFrontCharacter[0].w = 23;
+	this->tallWalkingFrontCharacter[0].h = 42;
+
+	this->tallWalkingFrontCharacter[1].x = 26;
+	this->tallWalkingFrontCharacter[1].y = 2;
+	this->tallWalkingFrontCharacter[1].w = 23;
+	this->tallWalkingFrontCharacter[1].h = 42;
+
+	this->tallWalkingFrontCharacter[2].x = 51;
+	this->tallWalkingFrontCharacter[2].y = 2;
+	this->tallWalkingFrontCharacter[2].w = 23;
+	this->tallWalkingFrontCharacter[2].h = 42;
+
+	this->tallWalkingFrontCharacter[3].x = 76;
+	this->tallWalkingFrontCharacter[3].y = 2;
+	this->tallWalkingFrontCharacter[3].w = 23;
+	this->tallWalkingFrontCharacter[3].h = 42;
+
+	this->tallWalkingFrontCharacter[4].x = 101;
+	this->tallWalkingFrontCharacter[4].y = 2;
+	this->tallWalkingFrontCharacter[4].w = 23;
+	this->tallWalkingFrontCharacter[4].h = 42;
+
+	this->tallWalkingFrontCharacter[5].x = 126;
+	this->tallWalkingFrontCharacter[5].y = 2;
+	this->tallWalkingFrontCharacter[5].w = 23;
+	this->tallWalkingFrontCharacter[5].h = 42;
+}
+
+
+
+void Character::loadTallWalkingBackSprite() {
+	this->tallWalkingBackCharacter[0].x = 1;
+	this->tallWalkingBackCharacter[0].y = 48;
+	this->tallWalkingBackCharacter[0].w = 23;
+	this->tallWalkingBackCharacter[0].h = 42;
+
+	this->tallWalkingBackCharacter[1].x = 26;
+	this->tallWalkingBackCharacter[1].y = 48;
+	this->tallWalkingBackCharacter[1].w = 23;
+	this->tallWalkingBackCharacter[1].h = 42;
+
+	this->tallWalkingBackCharacter[2].x = 51;
+	this->tallWalkingBackCharacter[2].y = 48;
+	this->tallWalkingBackCharacter[2].w = 23;
+	this->tallWalkingBackCharacter[2].h = 42;
+
+	this->tallWalkingBackCharacter[3].x = 76;
+	this->tallWalkingBackCharacter[3].y = 48;
+	this->tallWalkingBackCharacter[3].w = 23;
+	this->tallWalkingBackCharacter[3].h = 42;
+
+	this->tallWalkingBackCharacter[4].x = 101;
+	this->tallWalkingBackCharacter[4].y = 48;
+	this->tallWalkingBackCharacter[4].w = 23;
+	this->tallWalkingBackCharacter[4].h = 42;
+
+	this->tallWalkingBackCharacter[5].x = 126;
+	this->tallWalkingBackCharacter[5].y = 48;
+	this->tallWalkingBackCharacter[5].w = 23;
+	this->tallWalkingBackCharacter[5].h = 42;
+}
+
+
+void Character::loadTallWalkingLeftSprite() {
+	this->tallWalkingLeftCharacter[0].x = 1;
+	this->tallWalkingLeftCharacter[0].y = 93;
+	this->tallWalkingLeftCharacter[0].w = 23;
+	this->tallWalkingLeftCharacter[0].h = 42;
+
+	this->tallWalkingLeftCharacter[1].x = 23;
+	this->tallWalkingLeftCharacter[1].y = 93;
+	this->tallWalkingLeftCharacter[1].w = 23;
+	this->tallWalkingLeftCharacter[1].h = 42;
+
+	this->tallWalkingLeftCharacter[2].x = 49;
+	this->tallWalkingLeftCharacter[2].y = 93;
+	this->tallWalkingLeftCharacter[2].w = 23;
+	this->tallWalkingLeftCharacter[2].h = 42;
+
+	this->tallWalkingLeftCharacter[3].x = 74;
+	this->tallWalkingLeftCharacter[3].y = 93;
+	this->tallWalkingLeftCharacter[3].w = 23;
+	this->tallWalkingLeftCharacter[3].h = 42;
+
+	this->tallWalkingLeftCharacter[4].x = 99;
+	this->tallWalkingLeftCharacter[4].y = 93;
+	this->tallWalkingLeftCharacter[4].w = 23;
+	this->tallWalkingLeftCharacter[4].h = 42;
+}
+
+
+void Character::loadTallWalkingRightSprite() {
+	this->tallWalkingRightCharacter[0].x = 1;
+	this->tallWalkingRightCharacter[0].y = 138;
+	this->tallWalkingRightCharacter[0].w = 23;
+	this->tallWalkingRightCharacter[0].h = 42;
+
+	this->tallWalkingRightCharacter[1].x = 29;
+	this->tallWalkingRightCharacter[1].y = 138;
+	this->tallWalkingRightCharacter[1].w = 23;
+	this->tallWalkingRightCharacter[1].h = 42;
+
+	this->tallWalkingRightCharacter[2].x = 54;
+	this->tallWalkingRightCharacter[2].y = 138;
+	this->tallWalkingRightCharacter[2].w = 23;
+	this->tallWalkingRightCharacter[2].h = 42;
+
+	this->tallWalkingRightCharacter[3].x = 78;
+	this->tallWalkingRightCharacter[3].y = 138;
+	this->tallWalkingRightCharacter[3].w = 23;
+	this->tallWalkingRightCharacter[3].h = 42;
+
+	this->tallWalkingRightCharacter[4].x = 103;
+	this->tallWalkingRightCharacter[4].y = 138;
+	this->tallWalkingRightCharacter[4].w = 23;
+	this->tallWalkingRightCharacter[4].h = 42;
 }
