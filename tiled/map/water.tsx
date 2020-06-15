8eaa57c9463fcccde8d0b9ad4cd215957c22b60c
netaneldj <njamilis@fi.uba.ar>
2020-06-15 09:37:44 -0300

    Agrego la carpeta de tiled

---
+++ tiled/map/water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="24082" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../source/images/Suelos/24082.png" width="128" height="128"/>
+</tileset>
