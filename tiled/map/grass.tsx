012f8860acefb2462e1392864071a92d9aea3426
tszemzo <szemzotomas@gmail.com>
2020-06-21 18:02:14 -0300

    Tiled folder deleted

--- tiled/map/grass.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="24083" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="../source/images/Suelos/24083.png" width="128" height="128"/>
-</tileset>
--- tiled/map/minimap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="25" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="../source/images/Suelos/25.bmp" width="96" height="96"/>
-</tileset>
